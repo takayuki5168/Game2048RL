96f0c79feee16c84166cde472e67fd77aa836ee1
Takayuki Murooka <takayuki5168@gmail.com>
2020-01-04 23:31:06 +0900

    change model of dqn

--- policy/chainer/dqn.py
+++ policy/chainer/dqn.py
@@ -15,27 +15,27 @@ class QFunction(chainer.Chain):
     def __init__(self, obs_size, n_actions):
         super(QFunction, self).__init__(##python2.x用
         #super().__init__(#python3.x用
-            l0=L.Linear(obs_size, 100),
-            l1=L.Linear(100, 100),
-            l2=L.Linear(100, 100),
-            l3=L.Linear(100, 50),
-            l4=L.Linear(50, 50),
-            l5=L.Linear(50, 10),
-            l6=L.Linear(10, n_actions))
+            conv1 = L.Convolution2D(None, 128, ksize=(1, 2)),
+            conv2 = L.Convolution2D(None, 128, ksize=(2, 1)),
+            conv3 = L.Convolution2D(None, 128, ksize=(2, 2)),
+
+            l1=L.Linear(None, 100),
+            l2=L.Linear(None, 100),
+            l3=L.Linear(None, n_actions))
 
     def __call__(self, x, test=False):
         """
         x ; 観測#ここの観測って、stateとaction両方？
         test : テストモードかどうかのフラグ
         """
-        h = F.relu(self.l0(x)) #活性化関数は自分で書くの？
+        h = F.relu(self.conv1(x))
+        h = F.relu(self.conv2(x))
+        #pool1 = F.max_pooling_2d(conv1_2, ksize=2, stride=2)
+        h = F.relu(self.conv3(h))
         h = F.relu(self.l1(h))
         h = F.relu(self.l2(h))
-        h = F.relu(self.l3(h))
-        h = F.relu(self.l4(h))
-        h = F.relu(self.l5(h))
 
-        return chainerrl.action_value.DiscreteActionValue(self.l6(h))
+        return chainerrl.action_value.DiscreteActionValue(self.l3(h))
 
 obs_size = env.observation_space.shape[0]
 n_actions = env.action_space.n
@@ -45,8 +45,8 @@ optimizer = chainer.optimizers.Adam(eps=1e-1)
 optimizer.setup(q_func) #設計したq関数の最適化にAdamを使う
 gamma = 0.95
 explorer = chainerrl.explorers.ConstantEpsilonGreedy(
-    epsilon=0.1, random_action_func=env.action_space.sample)
-replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity = 10**6)
+    epsilon=0.5, random_action_func=env.action_space.sample)
+replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity = 6000) #10**6)
 phi = lambda x:x.astype(np.float32, copy=False)##型の変換(chainerはfloat32型。float64は駄目)
 
 agent = chainerrl.agents.DoubleDQN(
@@ -58,11 +58,11 @@ agent = chainerrl.agents.DoubleDQN(
 import time
 n_episodes = 1000
 start = time.time()
+R = 0  # return (sum of rewards)
 for i in range(1, n_episodes + 1):
     obs = env.reset()
     reward = 0
     done = False
-    R = 0  # return (sum of rewards)
     t = 0  # time step
     while not done:
         # 動きを見たければここのコメントを外す
@@ -73,11 +73,12 @@ for i in range(1, n_episodes + 1):
         t += 1
     if i % 10 == 0:
         print('episode:', i,
-              'R:', R,
+              'R:', R / 10.0,
               'statistics:', agent.get_statistics())
+        R = 0  # return (sum of rewards)
     agent.stop_episode_and_train(obs, reward, done)
 print('Finished, elapsed time : {}'.format(time.time()-start))
-
+agent.save("hoge")
 
 
 ## test
