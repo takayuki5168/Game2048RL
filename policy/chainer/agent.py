f6710ad665d93f0fbb9a2c3fda275b7d6be0832f
Takayuki Murooka <takayuki5168@gmail.com>
2020-01-05 23:17:56 +0900

    some refactoring

---
+++ policy/chainer/agent.py
@@ -0,0 +1,77 @@
+# coding: utf-8
+
+import chainer
+import chainerrl
+import numpy as np
+
+from game2048 import Game2048Env
+
+from dqn import QFunction
+
+class Agent(object):
+    def __init__(self):
+        self.env = Game2048Env(False)
+
+        obs_size = self.env.observation_space.shape[0]
+        n_actions = self.env.action_space.n
+        q_func = QFunction(obs_size, n_actions)
+
+        optimizer = chainer.optimizers.Adam(eps=1e-1)
+        optimizer.setup(q_func) #設計したq関数の最適化にAdamを使う
+        gamma = 0.95
+        # explorer = chainerrl.explorers.ConstantEpsilonGreedy(
+        #     epsilon=0.5, random_action_func=env.action_space.sample)
+        explorer = chainerrl.explorers.LinearDecayEpsilonGreedy(
+            start_epsilon=0.9, end_epsilon=0.2, decay_steps=1000 * 100, random_action_func=self.env.action_space.sample)
+        replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10**6)
+        phi = lambda x:x.astype(np.float32, copy=False)##型の変換(chainerはfloat32型。float64は駄目)
+
+        self.agent = chainerrl.agents.DoubleDQN(
+            q_func, optimizer, replay_buffer, gamma, explorer,
+            replay_start_size=500, update_interval=1,
+            target_update_interval=1000, phi=phi)
+
+    def train(self, model_path='sample'):
+        print("[Train]")
+        import time
+        n_episodes = 1000
+        start = time.time()
+        R = 0  # return (sum of rewards)
+        for i in range(1, n_episodes + 1):
+            obs = self.env.reset()
+            reward = 0
+            done = False
+            t = 0  # time step
+            while not done:
+                # 動きを見たければここのコメントを外す
+                self.env.render()
+                action = self.agent.act_and_train(obs, reward)
+                obs, reward, done, _ = self.env.step(action)
+                R += reward
+                t += 1
+            if i % 10 == 0:
+                print('episode:', i,
+                      'R:', R / 10.0,
+                      'statistics:', self.agent.get_statistics())
+                R = 0  # return (sum of rewards)
+            self.agent.stop_episode_and_train(obs, reward, done)
+        print('Finished, elapsed time : {}'.format(time.time()-start))
+
+        self.agent.save(model_path)   # save model
+
+    def test(self, model_path='sample'):
+        print("[Test]")
+        self.agent.load(model_path)   # load model
+
+        self.agent.explorer.epsilon = 0   # TODO is itTrue?
+
+        self.env.game.make_window()
+        while True:
+            obs = self.env.reset()
+            reward = 0
+            done = False
+            while not done:
+                self.env.render()
+                action = self.agent.act_and_train(obs, reward)
+                obs, reward, done, _ = self.env.step(action)
+            self.env.close()
--- policy/chainer/dqn.py
+++ policy/chainer/dqn.py
@@ -4,12 +4,6 @@ import chainer
 import chainer.functions as F
 import chainer.links as L
 import chainerrl
-import numpy as np
-
-from game2048 import Game2048Env
-
-env = Game2048Env(False)
-
 
 class QFunction(chainer.Chain):
     def __init__(self, obs_size, n_actions):
@@ -62,62 +56,3 @@ class QFunction(chainer.Chain):
         h = F.relu(self.l2(h))
 
         return chainerrl.action_value.DiscreteActionValue(self.l3(h))
-
-obs_size = env.observation_space.shape[0]
-n_actions = env.action_space.n
-q_func = QFunction(obs_size, n_actions)
-
-optimizer = chainer.optimizers.Adam(eps=1e-1)
-optimizer.setup(q_func) #設計したq関数の最適化にAdamを使う
-gamma = 0.95
-# explorer = chainerrl.explorers.ConstantEpsilonGreedy(
-#     epsilon=0.5, random_action_func=env.action_space.sample)
-explorer = chainerrl.explorers.LinearDecayEpsilonGreedy(
-    start_epsilon=0.9, end_epsilon=0.2, decay_steps=1000 * 100, random_action_func=env.action_space.sample)
-replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10**6)
-phi = lambda x:x.astype(np.float32, copy=False)##型の変換(chainerはfloat32型。float64は駄目)
-
-agent = chainerrl.agents.DoubleDQN(
-    q_func, optimizer, replay_buffer, gamma, explorer,
-    replay_start_size=500, update_interval=1,
-    target_update_interval=1000, phi=phi)
-
-
-import time
-n_episodes = 1000
-start = time.time()
-R = 0  # return (sum of rewards)
-for i in range(1, n_episodes + 1):
-    obs = env.reset()
-    reward = 0
-    done = False
-    t = 0  # time step
-    while not done:
-        # 動きを見たければここのコメントを外す
-        env.render()
-        action = agent.act_and_train(obs, reward)
-        obs, reward, done, _ = env.step(action)
-        R += reward
-        t += 1
-    if i % 10 == 0:
-        print('episode:', i,
-              'R:', R / 10.0,
-              'statistics:', agent.get_statistics())
-        R = 0  # return (sum of rewards)
-    agent.stop_episode_and_train(obs, reward, done)
-print('Finished, elapsed time : {}'.format(time.time()-start))
-agent.save("hoge")
-
-
-## test
-print("TEST")
-env.game.make_window()
-while True:
-    obs = env.reset()
-    reward = 0
-    done = False
-    while not done:
-        env.render()
-        action = agent.act_and_train(obs, reward)
-        obs, reward, done, _ = env.step(action)
-    env.close()
